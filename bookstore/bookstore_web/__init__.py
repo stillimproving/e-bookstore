81532e6164df1ef18a8bab9655a89446767bbc9b
stillimproving <xkasienka@gmail.com>
2020-05-06 14:20:05 +0200

    Initialization

--- bookstore/bookstore_web/__init__.py
+++ bookstore/bookstore_web/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config import Config
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from flask_login import LoginManager
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 logger = app.logger
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# login = LoginManager(app)
+# login.login_view = 'login'
 
 from bookstore.bookstore_web import routes
+# from bookstore import models
--- bookstore/bookstore_web/routes.py
+++ bookstore/bookstore_web/routes.py
@@ -1,9 +1,34 @@
+from flask import render_template, flash, redirect, url_for
 from bookstore.bookstore_web import app
-from flask import render_template
+from bookstore.bookstore_web.forms import LoginForm
+# from flask_login import logout_user
+# from flask_login import login_required
 
+NAME = 'e-Bookstore'
 
 @app.route('/')
+@app.route('/index')
+# @login_required
 def index():
-    user = {'username': 'Błażej'}
-    title = 'Księgania LoremIpsum'
-    return render_template('index.html', title=title, user=user)
+    return render_template('index.html', global_title=NAME, after_title=" | Home")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.usermail.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', global_title=NAME, after_title=' | Log In', form=form)
+
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
+
+@app.route('/terms_of_use')
+def terms_of_use():
+    return render_template('terms_of_use.html', global_title=NAME, after_title=' | Terms of use')
+
+@app.route('/privacy_policy')
+def privacy_policy():
+    return render_template('privacy_policy.html', global_title=NAME, after_title=' | Privacy policy')
